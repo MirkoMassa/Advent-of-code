1a93b9bd7d1c2fc2a8e83580a3954acaf5939e53
Mirko <mirkomassa18@gmail.com>
2022-12-03 22:48:11 +0100

    Day 3, part 2

--- day3/index.ts
+++ day3/index.ts
@@ -1,4 +1,5 @@
 //https://adventofcode.com/2022/day/3
+import { group } from 'console';
 import fs from 'fs'
 // Ascii codes
 // a : 97 | z : 122
@@ -15,7 +16,7 @@ export function rucksack(file:string): number{
     // console.log(priorities)
 
     //second half
-    let content:string[] = fs.readFileSync(file, 'utf-8').split("\r\n");;
+    let content:string[] = fs.readFileSync(file, 'utf-8').split("\r\n");
     //first half
     let firsthalf:string[] = content.map(elem => elem.slice(0, elem.length/2));
     content = content.map(elem => elem.slice(elem.length/2, elem.length));
@@ -46,8 +47,54 @@ export function rucksack(file:string): number{
 
     });
 
+    //return sum; // part 1 output
+
+    //part 2 - yet another naive solution 
+    content = fs.readFileSync(file, 'utf-8').split("\r\n");
+    sum = 0;
+
+    // nonsense 
+
+    // for(let i = 0;i<content.length;i+=3){
+
+    //     const groupOf3Set = new Set();
+
+    //     //adding group of 3 words and checking badge
+    //     for(let j = 0; j<3;j++){
+            
+    //         //adding 1 word
+    //         for(let k = 0;k<content[i+j].length;k++){
+
+    //             groupOf3Set.add(content[i+j][k]);
+    //             console.log(content[i+j][k])
+    //         }
+    //         console.log("word ",(j+i))
+    //     }
+
+
+    // }
+
+    for(let i = 0;i<content.length;i+=3){
+
+        let groupOf3:string[] = []; 
+
+        //grouping 3 words
+        for(let j = 0; j<3;j++){
+            
+            groupOf3.push(content[i+j]);
+            console.log("word ",(j+i))
+        }
+        //checking common chars
+        for(let j = 0; j<groupOf3[0].length;j++){
+            if(groupOf3[1].includes(groupOf3[0][j]) && groupOf3[2].includes(groupOf3[0][j])){
+                sum += priorities[groupOf3[0][j]];
+                break;
+            }
+        }
+
+    }
     return sum;
 }
 
-// rucksack('./test.txt')
-console.log(rucksack('./input.txt'))
+// console.log(rucksack('./test.txt'));
+console.log(rucksack('./input.txt'));
