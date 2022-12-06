fccff16cd0bed39c362e0d85947584ffa689f120
Mirko <mirkomassa18@gmail.com>
2022-12-06 08:23:31 +0100

    Day 6, part 2

--- day6/index.ts
+++ day6/index.ts
@@ -2,16 +2,22 @@ import fs from 'fs'
 
 export function tuningTrouble(file:string):number | undefined{
     let content = fs.readFileSync(file, 'utf-8');
-    
+
     for(let i = 0; i<content.length; i++){
         const group = new Set();
 
+        //start of packet marker
         for(let j = 0; j<4; j++){
             group.add(content[i+j]);
         }
-        if(group.size == 4) return i+4;
-    }
+        //if(group.size == 4) return i+4; // part 1 output
 
+        //part 2, start of message marker 
+        for(let j = 0; j<14; j++){
+            group.add(content[i+j]);
+        }
+        if(group.size == 14) return i+14;
+    }
 }
-// tuningTrouble('./test.txt');
+// console.log(tuningTrouble('./test.txt'));
 console.log(tuningTrouble('./input.txt'));
