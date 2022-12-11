2ff74d6b4b31f74e7f0479407e82cb134f181b47
Mirko <mirkomassa18@gmail.com>
2022-12-11 17:11:01 +0100

    Day 8, part 2

--- day8/index.ts
+++ day8/index.ts
@@ -21,56 +21,110 @@ export function Treetop(file:string){
     console.log("inner",inner)
     console.log("content",content)
 
-    inner.forEach((trees:number[], index:number)=>{
+    // inner.forEach((trees:number[], index:number)=>{
         
-        visibles+= trees.reduce((total:number, currtree:number, i:number, trees:number[])=>{
-            let isnotvisible = 0; //when equal to 4 the tree is not visible from any direction
-            const row = content[index+1];
-            console.log("riga",row)
-            //checking left
-            for (let j = 0; j < i+1; j++) {
+        // visibles+= trees.reduce((total:number, currtree:number, i:number, trees:number[])=>{
+        //     let isnotvisible = 0; //when equal to 4 the tree is not visible from any direction
+        //     const row = content[index+1];
+        //     console.log("riga",row)
+        //     //checking left
+        //     for (let j = 0; j < i+1; j++) {
                 
-                if(row[j] >= currtree){
-                    // console.log(currtree+" sinistra")
-                    isnotvisible++;
+        //         if(row[j] >= currtree){
+        //             // console.log(currtree+" sinistra")
+        //             isnotvisible++;
+        //             break;
+        //         }
+        //     }
+        //     //checking right
+        //     for (let j = i+2; j < row.length; j++) {
+
+        //         if(row[j] >= currtree){
+        //             // console.log(currtree+" destra")
+        //             isnotvisible++;
+        //             break;
+        //         }
+        //     }
+        //     //checking up
+        //     for (let j = 0; j <= index; j++) {
+
+        //         if(content[j][i+1] >= currtree){
+        //             // console.log(currtree+" su")
+        //             isnotvisible++;
+        //             break;
+        //         }
+        //     }
+        //     //checking down
+        //     for (let j = index+2 ; j < content.length; j++) {
+
+        //         if(content[j][i+1] >= currtree){
+        //             // console.log(currtree+" giu")
+        //             isnotvisible++;
+        //             break;
+        //         }
+        //     }
+        //     if(isnotvisible != 4){
+                
+        //         total++;
+        //     }
+        //     return total;
+        // }, 0)
+    // })
+    // return visibles; //part 1 output
+
+    //part 2
+    let scenicMax = -Infinity;
+    inner.forEach((trees:number[], index:number)=> {
+        
+        trees.forEach((tree, i)=>{
+            let scenicity = 1;
+            let distance = 0;
+            const row = content[index+1];
+
+            //checking left
+            for (let j = i; j >= 0; j--) {
+                
+                distance++;
+                if(row[j] >= tree){
                     break;
                 }
             }
+            scenicity *= distance;
+            distance = 0;
             //checking right
             for (let j = i+2; j < row.length; j++) {
-
-                if(row[j] >= currtree){
-                    // console.log(currtree+" destra")
-                    isnotvisible++;
+                distance++;
+                if(row[j] >= tree){
                     break;
                 }
             }
+            scenicity *= distance;
+            distance = 0;
             //checking up
-            for (let j = 0; j <= index; j++) {
-
-                if(content[j][i+1] >= currtree){
-                    // console.log(currtree+" su")
-                    isnotvisible++;
+            for (let j = index; j >= 0 ; j--) {
+                distance++;
+                if(content[j][i+1] >= tree){
                     break;
                 }
             }
+            scenicity *= distance;
+            distance = 0;
             //checking down
             for (let j = index+2 ; j < content.length; j++) {
-
-                if(content[j][i+1] >= currtree){
-                    // console.log(currtree+" giu")
-                    isnotvisible++;
+                distance++;
+                if(content[j][i+1] >= tree){
                     break;
                 }
             }
-            if(isnotvisible != 4){
-                
-                total++;
+            scenicity *= distance;
+
+            if(scenicMax < scenicity){
+                scenicMax = scenicity;
             }
-            return total;
-        }, 0)
-    })
-    console.log("totale visibili: "+ visibles)
+        })
+    });
+
+    return scenicMax;
 }
 
 // console.log(Treetop('./test.txt'));
