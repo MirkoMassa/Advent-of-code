b79d797a6f6ec6e2890a78c3ed0fc281f5f184ad
Mirko <mirkomassa18@gmail.com>
2022-12-16 18:31:46 +0100

    Day 12 part 1

--- day12/index.ts
+++ day12/index.ts
@@ -8,6 +8,7 @@ elevations['S'] = 1;
 elevations['E'] = 27;
 //S (my starting position) has elevation a, E (goal) has elevation z
 
+type coordinates = {x:number, y:number};
 
 class mapNode {
 
@@ -17,16 +18,19 @@ class mapNode {
     public down:mapNode | null;
     public left:mapNode | null;
     public right:mapNode | null;
+    public distance:number;
 
-    constructor(public elevation:number)
+    constructor(public elevation:number, public coordinates:coordinates)
     {
         this.elevation = elevation;
+        this.coordinates = coordinates;
         this.up = null;
         this.down = null;
         this.left = null;
         this.right = null;
 
         this.isEnd = false;
+        this.distance = Infinity;
     }
 }
 
@@ -40,6 +44,7 @@ function hillClimbing(file:string){
         if (char == '\n') dups++;
         return dups;
     }, 1)
+
     hillMapUnparsed = hillMapUnparsed.filter(elem => elem != '\n');
     let hillMapChars:string[][] = [];
     for (let i = 0; i < columnslength*rowlength; i+=rowlength) {
@@ -66,55 +71,86 @@ function hillClimbing(file:string){
     //     })
     // })
 
-    //this will be the node x:-1, y:0
-    const root = new mapNode(-1);
-    let currnode: mapNode | null = root;
-
     let starting:mapNode;
-    hillMapChars.forEach((row, ir) =>{ //ir = row
-
+    let currnode:mapNode;
+    let nodemap = hillMapChars.map((row, ir) =>{ //ir = row
         // current node selector after each row
-        if(ir>0) currnode = root.right;
 
-        row.forEach((hill, ih)=>{ //ih = column
-            if(currnode == root){ //just on the first cycle, referencing hill (0,0)
-                root.right = new mapNode(elevations[hill]);
-                currnode = root.right;
-                console.log("root ",root);
-            }
-            else currnode = new mapNode(elevations[hill]);
+        return row.map((hill, ih)=>{ //ih = column
+
+            currnode = new mapNode(elevations[hill], {x:ir, y:ih});
             if(hill == 'S') starting = currnode;
             if(hill == 'E') currnode.isEnd = true;
-            let up:mapNode | null, down:mapNode | null, left:mapNode | null, right:mapNode | null;
-            up = down = left = right = null;
-            let char;
-            if(ir != 0){
-                char = hillMapChars[ir-1][ih];
-                if(elevations[char] <= elevations[hill]+1) up = new mapNode(elevations[char]);
-            } 
-            if(ir != columnslength-1){
-                char = hillMapChars[ir+1][ih];
-                console.log("down", char)
-                if(elevations[char] <= elevations[hill]+1) down = new mapNode(elevations[char]);
-            } 
-            if(ih != 0){
-                char = hillMapChars[ir][ih-1];
-                if(elevations[char] <= elevations[hill]+1) left = new mapNode(elevations[char]);
-            } 
-            if(ih != rowlength){
-                char = hillMapChars[ir][ih+1];
-                if(elevations[char] <= elevations[hill]+1) right = new mapNode(elevations[char]);
-            }
-            currnode!.up = up;
-            currnode!.down = down;
-            currnode!.left = left;
-            currnode!.right = right;
+            
+            return currnode;
         })
     })
+    nodemap.forEach((row, ir)=>{
+        row.forEach((hill, ih)=>{
+            //Check the neighboring nodemap node if its height is no more than 1 above
+            //if so then add a connection, otherwise move on
+            
+            if(ir != 0){
+                if(nodemap[ir-1][ih].elevation <= hill.elevation+1) hill.up = nodemap[ir-1][ih];
+            }
+            if(ir != columnslength-1){
+                if(nodemap[ir+1][ih].elevation <= hill.elevation+1) hill.down = nodemap[ir+1][ih];
+            }
+            if(ih != 0){
+                if(nodemap[ir][ih-1].elevation <= hill.elevation+1) hill.left = nodemap[ir][ih-1];
+            }
+            if(ih != rowlength-1){
+                if(nodemap[ir][ih+1].elevation <= hill.elevation+1) hill.right = nodemap[ir][ih+1];
+            }
+        })
+    })
+    let visited = new Set();
+    starting!.distance = 0;
+    let queue:mapNode[] = [];
+    let endnode:mapNode;
+    queue.push(starting!);
 
-    console.log(root)
-    
+    while(queue.length!=0){
+        const currnode:mapNode = queue.shift()!;
+        if(currnode.isEnd){
+            endnode = currnode;
+        }
+        if(currnode.up){
+            if(!visited.has(currnode.up)){
+                queue.push(currnode.up);
+                visited.add(currnode.up);
+            } 
+            currnode.up.distance = Math.min(currnode.distance+1, currnode.up.distance);
+            
+        }
+        if(currnode.down){
+            if(!visited.has(currnode.down)){
+                queue.push(currnode.down);
+                visited.add(currnode.down);
+            }
+            currnode.down.distance = Math.min(currnode.distance+1, currnode.down.distance);
+            
+        }
+        if(currnode.left){
+            if(!visited.has(currnode.left)){
+                queue.push(currnode.left);
+                visited.add(currnode.left);
+            } 
+            currnode.left.distance = Math.min(currnode.distance+1, currnode.left.distance);
+           
+        }
+        if(currnode.right){
+            if(!visited.has(currnode.right)){
+                queue.push(currnode.right);
+                visited.add(currnode.right);
+            }
+            currnode.right.distance = Math.min(currnode.distance+1, currnode.right.distance);
+            
+        }
+
+    }
+    return endnode!.distance;
 }
 
-console.log(hillClimbing('./test.txt'));
-// console.log(hillClimbing('./input.txt'));
+// console.log(hillClimbing('./test.txt'));
+console.log(hillClimbing('./input.txt'));
